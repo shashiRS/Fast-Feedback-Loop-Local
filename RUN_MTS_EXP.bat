fca5ddd2c64b199649590f0c14020f07032dd526
shashi <33113076+shashiRS@users.noreply.github.com>
2024-05-27 16:57:57 +0530

    Update RUN_MTS_EXP.bat

--- RUN_MTS_EXP.bat
+++ RUN_MTS_EXP.bat
@@ -17,5 +17,5 @@ pip install websocket-client==1.3.1
 
 set ECAL_DATA=conan_workarea\package.next.generic.0.0.0-fallback.vs2017\bin
 set PYTHONPATH=%PYTHONPATH%;%1"\src\py_api"
-py src\next_py_api\next_api.py -c %2
+py src\py_api\next_py_api\next_api.py -c %2
 popd
