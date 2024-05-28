047cc0aba21729c018e6e88f79577695daf592a7
shashi <33113076+shashiRS@users.noreply.github.com>
2024-05-28 13:27:29 +0530

    Update RUN_MTS_EXP.bat

--- RUN_MTS_EXP.bat
+++ RUN_MTS_EXP.bat
@@ -1,22 +1,26 @@
 @echo off
 
+set WORKSPACE_PATH=%CD%
+
 echo "creating venv.."
-call %1"\src/py_api/install.bat"
+echo %CD%
+call "%CD%/src/py_api/install.bat"
 echo "successful"
 
 
 echo "activate venv.."
-call %1"\src/py_api/venv/Scripts/activate"
+echo %CD%
+call "%CD%/venv/Scripts/activate"
 echo "successful"
 
-py -m pip install src\py_api\dist\ecal5-5.13.0-cp39-cp39-win_amd64.whl
-py -m pip install src\py_api\dist\next_py_api-0.14.0-py3-none-any.whl
+py -m pip install %CD%/dist/ecal5-5.13.0-cp39-cp39-win_amd64.whl
+py -m pip install %CD%/dist/next_py_api-0.14.0-py3-none-any.whl
 pip install jsonschema
 pip install bs4
 pip install websocket-client==1.3.1
 
 set ECAL_DATA=conan_workarea\package.next.generic.0.0.0-fallback.vs2017\bin
-set PYTHONPATH=%PYTHONPATH%;%1"\src\py_api"
+set PYTHONPATH=%PYTHONPATH%;%CD%
 echo %2
-py src\py_api\next_py_api\next_api.py -c %2
+py echo %CD%\next_py_api\next_api.py -c %2
 popd
