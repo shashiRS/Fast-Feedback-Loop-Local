3317981f104fac7be9f9840c5769a1f3e3019722
shashi <33113076+shashiRS@users.noreply.github.com>
2024-05-28 09:23:12 +0530

    Update RUN_MTS_EXP.bat

--- RUN_MTS_EXP.bat
+++ RUN_MTS_EXP.bat
@@ -1,12 +1,12 @@
 @echo off
 
 echo "creating venv.."
-call %1"\next\src/py_api/install.bat"
+call %1"\src/py_api/install.bat"
 echo "successful"
 
 
 echo "activate venv.."
-call %1"\next\src/py_api/venv/Scripts/activate"
+call %1"\src/py_api/venv/Scripts/activate"
 echo "successful"
 
 py -m pip install src\py_api\dist\ecal5-5.13.0-cp39-cp39-win_amd64.whl
@@ -17,5 +17,6 @@ pip install websocket-client==1.3.1
 
 set ECAL_DATA=conan_workarea\package.next.generic.0.0.0-fallback.vs2017\bin
 set PYTHONPATH=%PYTHONPATH%;%1"\src\py_api"
+echo %2
 py src\py_api\next_py_api\next_api.py -c %2
 popd
